03797a2cc88917a69b82e47424da0e843c00a949
KIMINJOONG <ijboym216@gmail.com>
2022-08-04 22:01:38 +0900

    1. PageController 사용해보기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,19 +12,33 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Timer? timer;
+  PageController controller = PageController(initialPage: 0);
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
       print("Timer!");
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      controller.animateToPage(
+        nextPage,
+        duration: Duration(microseconds: 400),
+        curve: Curves.linearToEaseOut,
+      );
     });
   }
 
   @override
   void dispose() {
+    controller.dispose();
     if (timer != null) {
       timer!.cancel();
     }
@@ -33,8 +48,10 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1, 2, 3, 4, 5]
             .map((e) => Image.asset(
                   'asset/img/image_$e.jpeg',
